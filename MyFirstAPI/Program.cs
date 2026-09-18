14c01c3bbc2b9da7415aab6021fc2585cc27ae41
iFaustoo <faustoordoriguez10@gmail.com>
2026-09-18 20:39:32 -0300

    (feat): Adding two HTTP methods, GET and POST. The POST method was applied by creating a .http file in the directory to easily test the new data added

--- MyFirstAPI/Program.cs
+++ MyFirstAPI/Program.cs
@@ -1,6 +1,39 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! Creating my first API");
+
+var myTasks = new List<Task>
+{
+    new Task(1, "Creating an API in C#", true),
+    new Task(2, "Migrate the calculator", false),
+};
+
+app.MapGet("/tasks", () =>
+{
+   return myTasks; 
+});
+
+app.MapGet("tasks/{id}", (int id) =>
+{
+    var taskFound = myTasks.FirstOrDefault(t => t.Id == id);
+
+    if (taskFound != null)
+    {
+        return Results.Ok(taskFound);
+    }
+    else
+    {
+        return Results.NotFound("The id you were trying to match does not exist");
+    }
+});
+
+app.MapPost("/tasks", (Task newTask) =>
+{
+   myTasks.Add(newTask);
+   return myTasks;
+});
 
 app.Run();
+
+record Task(int Id, string Description, bool Completed);
