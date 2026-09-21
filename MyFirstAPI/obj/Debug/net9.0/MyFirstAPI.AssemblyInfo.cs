0fe74b0cca703e2a5d026edbe92ba5e67077c8d6
iFaustoo <faustoordoriguez10@gmail.com>
2026-09-21 16:10:16 -0300

    (feat): Integrated the Swashbuckle package into the project to automatically generate OpenAPI specifications. Configured and enabled Swagger UI to provide an interactive interface for endpoints visualization, documentation, and manual API testing during development.

--- MyFirstAPI/obj/Debug/net9.0/MyFirstAPI.AssemblyInfo.cs
+++ MyFirstAPI/obj/Debug/net9.0/MyFirstAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67592efbb4e8e124d495144e0ee4379a423677ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f8640c72fe4041469bd8ddb296e6c8278b7df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
